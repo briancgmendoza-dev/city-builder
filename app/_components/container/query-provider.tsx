15678c7d3c95356362bca313ddf46063cdaff374
Brian Christopher Mendoza <briancgmendoza@gmail.com>
2025-02-09 19:00:47 +0800

    [Bugfix] Fixing persist storage

--- app/_components/container/query-provider.tsx
+++ app/_components/container/query-provider.tsx
@@ -1,28 +1,20 @@
 'use client'
 
-import React, { Suspense, useEffect, useState } from "react"
+import React from "react"
 import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client"
 import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister"
 
-import Loader from "@/app/_components/ui/loader"
 
 import { queryClient } from "@/app/libs/react-query"
 import { TQueryClientProviderProps } from "@/app/_components/container/type"
 
 const QueryProvider: React.FC<TQueryClientProviderProps> = ({ children }) => {
-  const [persister, setPersister] = useState<ReturnType<typeof createSyncStoragePersister> | null>(null)
 
-  useEffect(() => {
-    setPersister(createSyncStoragePersister({ storage: window.localStorage }))
-  }, [])
-
-  if (!persister) return <Loader />
+  const persister = createSyncStoragePersister({ storage: typeof window !== 'undefined' ? window.localStorage : undefined })
 
   return (
     <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
-      <Suspense fallback={<Loader />}>
         {children}
-      </Suspense>
     </PersistQueryClientProvider>
   )
 }
