e624286b1371426a4d70c8e0a97ef5c9f4892a29
Brian Christopher Mendoza <briancgmendoza@gmail.com>
2025-02-09 16:22:55 +0800

    [UI] Adding borders in floor component

--- app/_components/house/floors.tsx
+++ app/_components/house/floors.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import { TFloorsProps } from "@/app/_components/house/types"
 
 const Floors = React.forwardRef<HTMLDivElement, TFloorsProps>(({ children, color, floorsClassName }, ref) => (
-  <div ref={ref} className={`w-[100px] ${floorsClassName}`} style={{ backgroundColor: color }}>
+  <div ref={ref} className={`w-[100px] border-2 border-black ${floorsClassName}`} style={{ backgroundColor: color }}>
     {children}
   </div>
 ))
